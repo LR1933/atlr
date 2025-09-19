5858349c6bc19a8e01dd3b88523fe0a25dd4bdd5
Li Jiaqi <107530058+LR1933@users.noreply.github.com>
2025-09-19 16:34:25 +0900

    Update thecodestack.R

--- R/thecodestack.R
+++ R/thecodestack.R
@@ -890,8 +890,8 @@ fe <- function(
     }
     if (!is.null(fe.rows)) {
         if (is.character(fe.rows)) {
-            t <- t[Factor %in% fe.rows]
-            t[, Factor := factor(Factor, levels = fe.rows)]
+            t <- t[grepl(paste(fe.rows, collapse = "|"), Factor)]
+            t[, Factor := factor(Factor, levels = unique(Factor))]
             setorder(t, Factor)
         } else if (is.numeric(fe.rows)) {
             t <- t[fe.rows]
